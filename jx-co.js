b1ebd6809e9147b0fbcc5a6832e123fd4e27b12e
jiaxin <924463684@qq.com>
2017-09-28 16:14:42 +0800

    js-02&jx-co

---
+++ jx-co.js
@@ -0,0 +1,16 @@
+//实现一个代码库
+var jxCo = {
+
+	// 数组升序排序
+	dyadicArraySort: function (x, y, type) {
+		switch (type) {
+			case 1:
+				return x - y;
+			case 2:
+				return x[1] - y[1];
+		}
+	},
+	
+
+
+}
