4058e75bddea048a7bdca65a8477bd170b62feed
jiaxin <924463684@qq.com>
2018-03-08 18:29:38 +0800

    函数

--- 渡一/loop/loop.js
+++ 渡一/loop/loop.js
@@ -24,8 +24,8 @@ function doFn3() {
         var result;
         if (n < 3) {
             result = 1;
-        }else{
-            result = sum(n - 1) + sum(n - 2);            
+        } else {
+            result = sum(n - 1) + sum(n - 2);
         }
         return result;
     }
@@ -34,9 +34,45 @@ function doFn3() {
 
 }
 
-function doFn4(){
+function doFn4() {
     var n = document.getElementById('number4').value;
     var result = n % 100 % 10 * 100 + parseInt(n % 100 / 10) * 10 + parseInt(n / 100);
-    
+
     document.getElementById('result4').innerHTML = result;
+}
+
+function doFn5() {
+    var n1 = document.getElementById('number5').value;
+    var n2 = document.getElementById('number6').value;
+    var n3 = document.getElementById('number7').value;
+
+    var result = [n1, n2, n3].sort(function (a, b) {
+        return b - a;
+    })
+    document.getElementById('result5').innerHTML = result[0];
+}
+
+//质数，仅能被1或自身整除的书
+function doFn6() {
+    var result = [];
+    for (var n = 1; n < 100; n++) {
+        result.push(n);                        
+        for (var i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                result.pop(n);                                
+                break;
+            } else {
+            }
+        }
+
+    }
+    document.getElementById('result6').innerHTML = result;
+
+}
+
+// 壹贰叁肆伍陆柒捌玖拾佰仟万
+function doFn4(){
+    var n = document.getElementById('number8').value;
+    
+
 }
